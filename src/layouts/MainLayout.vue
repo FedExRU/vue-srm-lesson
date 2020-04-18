60d8f0ab2ca21b95fd9d5ffadf10688faac1bfc7
Pavel Uryadyshev <p.uryadyshev@roseltorg.ru>
2020-04-18 16:16:58 +0300

    lesson-4: фильтрация даты + доделывание меню

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,28 +1,29 @@
 <template>
-    <div class="app-main-layout">
-      <Navbar @toggle-menu="isOpen = !isOpen"/>
-      <Sidebar v-model="isOpen"/>
-      <main class="app-content" :class="{full: !isOpen}">
-          <div class="app-page">
-            <router-view/>
-          </div>
-      </main>
-
-      <div class="fixed-action-btn">
-          <a class="btn-floating btn-large blue" href="#">
-            <i class="large material-icons">add</i>
-          </a>
+  <div class="app-main-layout">
+    <Navbar @toggle-menu="isOpen = !isOpen"/>
+    <Sidebar v-model="isOpen"/>
+    <main class="app-content" :class="{full: !isOpen}">
+      <div class="app-page">
+        <router-view/>
       </div>
-    </div>
+    </main>
+    <RecordFloatButton />
+  </div>
 </template>
 
+
+
 <script>
+
 import Navbar from '@/components/app/Navbar';
 import Sidebar from '@/components/app/Sidebar';
+import RecordFloatButton from "../components/app/RecordFloatButton";
+
 export default {
   name: 'mainLayout',
   components: {
     Navbar,
+    RecordFloatButton,
     Sidebar
   },
   data: () => ({
