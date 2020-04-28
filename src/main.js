a5de0e3b1bb5bc4a2c6861bf2f10ccaeeb5431f0
Pavel Uryadyshev <p.uryadyshev@roseltorg.ru>
2020-04-28 17:27:27 +0300

    lesson-7: подключение firebase

--- src/main.js
+++ src/main.js
@@ -8,18 +8,49 @@ import dateFilter from "./filters/date.filter";
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
+/*
+ * <<<<< Firebase start >>>>>
+ */
+import firebase from 'firebase/app';
+require('firebase/auth');
+require('firebase/database');
+/*
+ * <<<<<< Firebase end >>>>>>
+ */
+
 Vue.config.productionTip = false;
 Vue.filter('dateFilter', dateFilter);
 Vue.use(Vuelidate);
 Vue.use(VueFlashMessage, {
   messageOptions: {
-    timeout: 5000,
+    timeout: 3000,
     pauseOnInteract: true
   }
 });
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app');
+firebase.initializeApp({
+  apiKey: "AIzaSyCKr-bJefT73ioAfiwCgCdkF_SNPmC9WFA",
+  authDomain: "vue-crm-fedexru.firebaseapp.com",
+  databaseURL: "https://vue-crm-fedexru.firebaseio.com",
+  projectId: "vue-crm-fedexru",
+  storageBucket: "vue-crm-fedexru.appspot.com",
+  messagingSenderId: "979536273429",
+  appId: "1:979536273429:web:102cb760cf5dfa54fa2e1c",
+  measurementId: "G-2M40RBT9PH"
+});
+
+/*
+ * Mount app
+ */
+let app;
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app');
+  }
+});
+
+
